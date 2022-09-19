0ae8f7da675ac57ed253fab9b3b7287a14d724f3
efecetinalp <efecetinalp@gmail.com>
2022-09-19 22:41:34 +0300

    Core layer is created, EfEntityRepositoryBase is added, CarDetilDto is tested

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,14 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Business.Abstract;
 using Business.Concrete;
+using DataAccess.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Linq;
 
-CarManager carManager = new CarManager(new EfCarDal());
-
-
-
-
-foreach (var car in carManager.GetCarsByBrandId(1))
+namespace ConsoleUI
 {
-    Console.WriteLine(car.BrandId + " " + car.ModelYear + " " + car.DailyPrice);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarTest();
+
+            //BrandTest();
+
+            //ColorTest();
+
+        }
+
+        private static void ColorTest()
+        {
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+
+            foreach (var color in colorManager.GetAll())
+            {
+                Console.WriteLine(color.ColorName);
+            }
+        }
+
+        private static void BrandTest()
+        {
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+
+            foreach (var brand in brandManager.GetAll())
+            {
+                Console.WriteLine(brand.BrandName);
+            }
+        }
+
+        private static void CarTest()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            foreach (var car in carManager.GetCarDetails())
+            {
+                Console.WriteLine(car.CarName + " " + car.BrandName + " " + car.ColorName + " " + car.DailyPrice);
+            }
+        }
+    }
 }
+
+            
